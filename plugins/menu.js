0086d69da77b6c6af3bd1e4e4aa803916e8790d2
我们是 <78197555+mlspacetoon@users.noreply.github.com>
2021-02-20 06:56:41 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -125,9 +125,9 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
     throw e
   }
 }
-handler.help = ['on','start','?']
+handler.help = ['menu,'start','?']
 handler.tags = ['main']
-handler.command = /^(on|start|\?)$/i
+handler.command = /^(menu|start|\?)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
