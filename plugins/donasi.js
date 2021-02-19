a5ee621f5f9ae7500299c8fdde0d9c270b9e18ba
我们是 <78197555+mlspacetoon@users.noreply.github.com>
2021-02-19 17:42:53 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,8 +1,15 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [081515860089]
-│ • Telkomsel [081334177340]
-╰────
+let handler = async m => m.reply(`
+╔═──「 Mao • Donasikah? 」
+│-------------
+│ ❏ OVO  : 0877-1474-5440
+│ ❏ DANA : 0877-1474-5440
+│ ❏ PULSA: 0877-1474-5440
+│-------------
+│ ❏ Atw Bisa klik bit.ly/ClickDiSinii
+│ ❏ Atw juga inihh hhe saweria.co/PUDlDl
+│ ❏ Maaci>< mwhhehhehehehehehehehhe
+╚═─────────────────────────────────ᜰ
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
