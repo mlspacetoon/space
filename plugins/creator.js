e51b4c637fcf671291bffb882d08eddee8b804e0
我们是 <78197555+mlspacetoon@users.noreply.github.com>
2021-02-19 17:41:30 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
   // this.sendContact(m.chat, '6287714745440', 'Fadhli', m)
-  this.sendContact(m.chat, '0', 'Insert Owner Number Here', m)
+  this.sendContact(m.chat, '6287714746440', 'Fadhli', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
