d61bbe67e26353cdf09768ad7288602520b71a00
我们是 <78197555+mlspacetoon@users.noreply.github.com>
2021-02-19 17:51:59 +0700

    Update nulis.js

--- plugins/nulis.js
+++ plugins/nulis.js
@@ -51,7 +51,7 @@ let handler  = async (m, { conn, args }) => {
   ])
   .on('error', e => conn.reply(m.chat, util.format(e), m))
   .on('exit', () => {
-    conn.sendFile(m.chat, outputPath, 'nulis.jpg', 'Hati² ketahuan:v', m)
+    conn.sendFile(m.chat, outputPath, 'nulis.jpg', 'Niehh tulisannya.. Mager yaak?\n *Follow dung* instagram.com/itspapoy', m)
   })
 }
 handler.help = ['n'].map(v => v + 'ulis <teks>')
