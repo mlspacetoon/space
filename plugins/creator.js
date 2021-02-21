e7b906d4cba8ed052b0f9b65306bef32fbbb3a79
我们是 <78197555+mlspacetoon@users.noreply.github.com>
2021-02-21 07:42:33 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,9 +1,6 @@
 let handler = function (m) {
-let handler  = async (m, { conn }) => {
-  conn.reply(m.chat,`Ini Ownernyaa\nJanlup di sv yaa:>`, m)
   // this.sendContact(m.chat, '6287714745440', 'Fadhli', m)
   this.sendContact(m.chat, '6287714746440', 'Fadhli', m)
-this.conn.reply(m.chat, 'Ini Ownernyaa\nJanlup di sv yaa:>', m),
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
