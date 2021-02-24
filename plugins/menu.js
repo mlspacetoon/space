8dc73283fe66a768f57dd02326bed2376801253e
我们是 <78197555+mlspacetoon@users.noreply.github.com>
2021-02-25 06:57:04 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -100,7 +100,7 @@ Btw have a nice day:>
 %readmore`
     let header = conn.menu.header || '┌────── %category ──────•°҈ۣٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜٜۜ͜͡⸙ '
     let body   = conn.menu.body   || '│ ˚• ͟͟͞͞➳ ⌬ %cmd%islimit'
-    let footer = conn.menu.footer || '╰─────────────ᜰ\n'
+    let footer = conn.menu.footer || '└──────────────ˎˊ˗ᜰ\n'
     let after  = conn.menu.after  || (conn.user.jid == global.conn.user.jid ? '' : `Powered by https://wa.me/${global.conn.user.jid.split`@`[0]}`) + `\n*%npmname@^%version*\n\`\`\`\%npmdesc\`\`\``
     let _text  = before + '\n'
     for (let tag in groups) {
