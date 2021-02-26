1f444592e45d66895d7857a03aeea58bc9ece4a2
我们是 <78197555+mlspacetoon@users.noreply.github.com>
2021-02-26 19:31:42 +0700

    Update register.js

--- plugins/register.js
+++ plugins/register.js
@@ -1,6 +1,29 @@
 const { createHash } = require('crypto')
 let Reg = /(.*)([.|])([0-9]*)$/i
 let handler = async function (m, { text, usedPrefix }) {
+let d = new Date
+    let locale = 'id'
+    let gmt = new Date(0).getTime() - new Date('1 January 1970').getTime()
+    let week = d.toLocaleDateString(locale, { weekday: 'long' })
+    let date = d.toLocaleDateString(locale, {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    })
+    let time = d.toLocaleTimeString(locale, {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric'
+    })
+    let _uptime = process.uptime() * 1000
+    let _muptime
+    if (process.send) {
+      process.send('uptime')
+      _muptime = await new Promise(resolve => {
+        process.once('message', resolve)
+        setTimeout(resolve, 1000)
+      }) * 1000
+    }
   let user = global.DATABASE._data.users[m.sender]
   if (user.registered === true) throw `Kamu sudah terdaftar misqah\nMau daftar ulang? ${usedPrefix}unreg <SN|SERIAL NUMBER>`
   if (!Reg.test(text)) throw `Format salah\n*${usedPrefix}daftar <nama>.umur>*`
@@ -18,6 +41,7 @@ let handler = async function (m, { text, usedPrefix }) {
 ⏜⏜⏜⏜⏜⏜⏜⏜
 ⌲    *Informations* 
 ⏝⏝⏝⏝⏝⏝⏝⏝
+%week, %date
 ⏥⏥⏥⏥⏥⏥⏥⏥⏥⏥
 ------------------------------
 ⌬ \`\`\`Nama: ${name}\`\`\`
